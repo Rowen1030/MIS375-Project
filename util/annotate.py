a14a955de495a6741791a8fc660847cf541ffac0
Andy Pham <apham97@utexas.edu>
2018-11-25 15:30:11 -0600

    [US4] Update Annotation Script Dir function

--- util/annotate.py
+++ util/annotate.py
@@ -18,12 +18,22 @@ def get_maps():
         dirs[:] = [d for d in dirs if d not in exclude]
         for file in files:
             if file[len(file)-5:len(file)] == '.html':
-                maps_list.append(os.path.abspath(file))
+                maps_list.append(os.path.join(root,file))
         
     return maps_list
 def main():
     maps_list = get_maps()
-    print(maps_list)
+    print('\n')
+    print('''  ___                    _        _             
+ / _ \                  | |      | |            
+/ /_\ \_ __  _ __   ___ | |_ __ _| |_ ___  _ __ 
+|  _  | '_ \| '_ \ / _ \| __/ _` | __/ _ \| '__|
+| | | | | | | | | | (_) | || (_| | || (_) | |   
+\_| |_/_| |_|_| |_|\___/ \__\__,_|\__\___/|_|   ''')
+    print('---------------------------------------------------------------------')
+    print('Select a map to manage annotations for. Enter the Number Besides the Name\n')
+    for map in maps_list:
+        print(str(maps_list.index(map)) + '. ' + map)
 
 
 main()
