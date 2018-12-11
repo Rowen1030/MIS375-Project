97d9eac0ad0bf53a9ede2981824b5881bfe34995
root <root@pure-deploy.us-east1-b.c.d-web-interface.internal>
2018-12-11 15:41:40 +0000

    Enter message here

---
+++ interfaces/bonney/pointclouds/LakeBonney.html/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 48987,
+    "boundingBox": {
+        "lx": 1370694.0,
+        "ly": -437449.8125,
+        "lz": -50.61397933959961,
+        "ux": 1372867.3125,
+        "uy": -435276.5,
+        "uz": 2122.6985206604004
+    },
+    "tightBoundingBox": {
+        "lx": 1370694.0,
+        "ly": -437449.8125,
+        "lz": -50.61397933959961,
+        "ux": 1372100.0,
+        "uy": -435276.5,
+        "uz": -0.00368599989451468
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 18.82143783569336,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
